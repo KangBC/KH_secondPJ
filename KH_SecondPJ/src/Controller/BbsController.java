931b396489ff33774cea57b6888fb94efc16cefa
BIT\lenovo <Kang.b.c.1208@gmail.com>
2018-08-30 10:06:20 +0900

    Start

---
+++ KH_SecondPJ/src/Controller/BbsController.java
@@ -0,0 +1,30 @@
+package Controller;
+
+import java.io.IOException;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class BbsController extends HttpServlet {
+
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		doProcess(req, resp);
+	}
+
+	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		doProcess(req, resp);
+	}
+	
+	public void doProcess(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		
+	}
+
+	public void dispatch(String urls, HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException{
+		RequestDispatcher dispatch = req.getRequestDispatcher(urls);
+		
+		dispatch.forward(req, resp);
+	}
+}
